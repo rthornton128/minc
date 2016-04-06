ebdb35e495abcd2ec797965d827aec9c889ba24e
Rob Thornton <rthornton128@gmail.com>
2016-04-05 19:06:26 -0700

    mcc-linux: fix assign bug and name bug

--- mcc/main_linux.go
+++ mcc/main_linux.go
@@ -7,8 +7,8 @@ import (
 
 func execLinker(lnk, flags, name string) {
 	if flags == "" {
-		flags := fmt.Sprintf("-o %s -e main %s.o", name, name)
+		flags = fmt.Sprintf("-o %s -e main %s.o", name, name)
 	}
 	execProg(lnk, flags)
-	os.Remove(name)
+	os.Remove(name + ".o")
 }
