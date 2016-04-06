cb60bed88d993262655fa23a22dc3331eefc74d6
Rob Thornton <rthornton128@gmail.com>
2016-04-06 12:16:57 -0700

    cgen: commenting and more comprehensive testing

--- cgen/cgen.go
+++ cgen/cgen.go
@@ -1,12 +1,18 @@
+// Package cgen produces x86 instructions using Intel syntax. At current,
+// it is intended to produce code for the GAS (GNU Assembler) and is likely
+// not portable with other assemblers like: llvm assembler, nasm, masm, etc.
 package cgen
 
 import (
 	"fmt"
 	"io"
+	"os"
 
 	"github.com/rthornton128/minc/ast"
 )
 
+// CGen wraps an io.Writer to output the generated assembly to. If
+// Writer is nil, it defaults to stdout
 type CGen struct{ io.Writer }
 
 func (c *CGen) emit(msg string, args ...interface{}) {
@@ -17,14 +23,20 @@ func (c *CGen) emitPreamble() {
 	c.emit(".intel_syntax noprefix\n\n")
 }
 
-func (c *CGen) Function(f *ast.Function) {
+func (c *CGen) function(f *ast.Function) {
 	c.emit("%s:\n", f.Name.Lit)
 	c.emitExit(ExitSuccess) // always close with success
 }
 
+// Generate begins the code generation process.
 func (c *CGen) Generate(p *ast.Program) {
+	// ensure no panics ensue as a result of Writer being nil. output is sent
+	// to stdout
+	if c.Writer == nil {
+		c.Writer = os.Stdout
+	}
 	c.emitPreamble()
 	c.emit(".global %s\n\n", p.Function.Name.Lit)
 	c.emit(".section .text\n")
-	c.Function(p.Function)
+	c.function(p.Function)
 }
--- cgen/cgen_test.go
+++ cgen/cgen_test.go
@@ -2,7 +2,9 @@ package cgen_test
 
 import (
 	"bytes"
-	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
 	"strings"
 	"testing"
 
@@ -10,16 +12,58 @@ import (
 	"github.com/rthornton128/minc/parse"
 )
 
-func TestPreamble(t *testing.T) {
+func TestGenerateSanity(t *testing.T) {
 	src := strings.NewReader("void main() {}")
 
 	var p parse.Parser
 	p.Init("cgen.mc", src)
 
+	// compile program
+	prog := p.Parse()
+	buf1 := new(bytes.Buffer)
+	gen := cgen.CGen{buf1}
+	gen.Generate(prog)
+
+	// compile program again
+	buf2 := new(bytes.Buffer)
+	gen = cgen.CGen{buf2}
+	gen.Generate(prog)
+
+	// confirm results are the same
+	if bytes.Compare(buf1.Bytes(), buf2.Bytes()) != 0 {
+		t.Fatal("output from buf1 differs from buf2")
+	}
+}
+
+func TestGenerateAssemble(t *testing.T) {
+	src := strings.NewReader("void main() {}")
+
+	var p parse.Parser
+	p.Init("cgen.mc", src)
+
+	// compile program
 	prog := p.Parse()
 	buf := new(bytes.Buffer)
 	gen := cgen.CGen{buf}
 	gen.Generate(prog)
 
-	fmt.Println(buf)
+	if msg, err := execAssembler(buf); err != nil {
+		if msg != nil {
+			t.Log(string(msg))
+		}
+		t.Fatal(err)
+	}
+}
+
+func execAssembler(buf *bytes.Buffer) ([]byte, error) {
+	f, err := ioutil.TempFile("", "cgentest")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	cmd := exec.Command("as", "-o", f.Name())
+	cmd.Stdin = buf
+	return cmd.CombinedOutput()
 }
